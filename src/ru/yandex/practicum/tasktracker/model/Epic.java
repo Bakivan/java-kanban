3927d873e043cf76a0d3d10ff36170798bdc5583
Ivan Bakanov <Ivanbakanov999@yandex.ru>
2024-03-24 00:36:14 +0300

    Новая версия кода #5

--- src/ru/yandex/practicum/tasktracker/model/Epic.java
+++ src/ru/yandex/practicum/tasktracker/model/Epic.java
@@ -3,17 +3,17 @@ package ru.yandex.practicum.tasktracker.model;
 import java.util.ArrayList;
 
 public class Epic extends Task {
-    private ArrayList<Integer> sintaskIds = new ArrayList<>();
+    private ArrayList<Integer> subtaskIds = new ArrayList<>();
 
     public Epic(String name, String description, TaskStatus status, int id) {
         super(name, description, status, id);
     }
 
-    public ArrayList<Integer> getSintaskIds() {
-        return sintaskIds;
+    public ArrayList<Integer> getSubtaskIds() {
+        return subtaskIds;
     }
 
-    public void setSintaskIds(ArrayList<Integer> sintaskIds) {
-        this.sintaskIds = sintaskIds;
+    public void setSubtaskIds(ArrayList<Integer> subtaskIds) {
+        this.subtaskIds = subtaskIds;
     }
 }
